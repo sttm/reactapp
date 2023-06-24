ceb833be3f944e0484f6206b0fd40660eeb5a799
sttm <nikitasavenkov7@gmail.com>
2023-06-25 02:39:00 +0300

    version1

--- src/app.jsx
+++ src/app.jsx
@@ -153,7 +153,7 @@ const itemsPerPage = 10;
     loadAudio(track.uri);
     createAudioContext();
     setCurrentTrackIndex(trackIndex);
-    
+    setPlaybackRate(1);
   }
 
   function setCurrentTrackIndexFromPlayer(newIndex) {
